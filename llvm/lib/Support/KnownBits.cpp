fa7c226e236790b692ec0dfc1112d07e23f1722e
Yuyou Fan <spicaofsky@gmail.com>
2025-09-26 12:47:13 -0600

    Add init setting

--- llvm/lib/Support/KnownBits.cpp
+++ llvm/lib/Support/KnownBits.cpp
@@ -97,18 +97,18 @@ KnownBits KnownBits::computeForAddSub(bool Add, bool NSW, bool NUW,
   if (!LHS.isUnknown() && !RHS.isUnknown()) {
     if (Add) {
       // Sum = LHS + RHS + 0
-      //KnownOut = ::computeForAddCarry(LHS, RHS, /*CarryZero=*/true,
-      //                                /*CarryOne=*/false);
-      auto res = add_solution(LHS_vec, RHS_vec);
-      KnownOut= VecToKB(res);
+      KnownOut = ::computeForAddCarry(LHS, RHS, /*CarryZero=*/true,
+                                      /*CarryOne=*/false);
+      //auto res = add_solution(LHS_vec, RHS_vec);
+      //KnownOut= VecToKB(res);
     } else {
       // Sum = LHS + ~RHS + 1
-      auto res = sub_solution(LHS_vec, RHS_vec);
-      KnownOut= VecToKB(res);
-      //KnownBits NotRHS = RHS;
-      //std::swap(NotRHS.Zero, NotRHS.One);
-      //KnownOut = ::computeForAddCarry(LHS, NotRHS, /*CarryZero=*/false,
-       //                               /*CarryOne=*/true);
+      //auto res = sub_solution(LHS_vec, RHS_vec);
+      //KnownOut= VecToKB(res);
+      KnownBits NotRHS = RHS;
+      std::swap(NotRHS.Zero, NotRHS.One);
+      KnownOut = ::computeForAddCarry(LHS, NotRHS, /*CarryZero=*/false,
+                                     /*CarryOne=*/true);
     }
   }
 
@@ -316,7 +316,7 @@ static unsigned getMaxShiftAmount(const APInt &MaxValue, unsigned BitWidth) {
 KnownBits KnownBits::shl(const KnownBits &LHS, const KnownBits &RHS, bool NUW,
                          bool NSW, bool ShAmtNonZero) {
   unsigned BitWidth = LHS.getBitWidth();
-  auto newRHS = RHS.zextOrTrunc(LHS.getBitWidth());
+  /*auto newRHS = RHS.zextOrTrunc(LHS.getBitWidth());
   auto LHS_vec = KBToVec(LHS), RHS_vec = KBToVec(newRHS);
   if (NUW&&NSW){
     return VecToKB(shl_nsuw_solution(LHS_vec, RHS_vec));
@@ -326,7 +326,7 @@ KnownBits KnownBits::shl(const KnownBits &LHS, const KnownBits &RHS, bool NUW,
     return VecToKB(shl_nsw_solution(LHS_vec, RHS_vec));
   }else{
     return VecToKB(shl_solution(LHS_vec, RHS_vec));
-  }
+  }*/
   auto ShiftByConst = [&](const KnownBits &LHS, unsigned ShiftAmt) {
     KnownBits Known;
     bool ShiftedOutZero, ShiftedOutOne;
@@ -412,7 +412,7 @@ KnownBits KnownBits::shl(const KnownBits &LHS, const KnownBits &RHS, bool NUW,
 KnownBits KnownBits::lshr(const KnownBits &LHS, const KnownBits &RHS,
                           bool ShAmtNonZero, bool Exact) {
   unsigned BitWidth = LHS.getBitWidth();
-  auto newRHS = RHS.zextOrTrunc(LHS.getBitWidth());
+  /*auto newRHS = RHS.zextOrTrunc(LHS.getBitWidth());
   auto LHS_vec = KBToVec(LHS), RHS_vec = KBToVec(newRHS);
   if(Exact){
     auto res = lshrexact_solution(LHS_vec, RHS_vec);
@@ -420,7 +420,7 @@ KnownBits KnownBits::lshr(const KnownBits &LHS, const KnownBits &RHS,
   }else{
     auto res = lshr_solution(LHS_vec, RHS_vec);
     return VecToKB(res);
-  }
+  }*/
   auto ShiftByConst = [&](const KnownBits &LHS, unsigned ShiftAmt) {
     KnownBits Known = LHS;
     Known.Zero.lshrInPlace(ShiftAmt);
@@ -479,13 +479,13 @@ KnownBits KnownBits::lshr(const KnownBits &LHS, const KnownBits &RHS,
 KnownBits KnownBits::ashr(const KnownBits &LHS, const KnownBits &RHS,
                           bool ShAmtNonZero, bool Exact) {
   unsigned BitWidth = LHS.getBitWidth();
-  auto newRHS=RHS.zextOrTrunc(BitWidth);
+  /*auto newRHS=RHS.zextOrTrunc(BitWidth);
   auto LHS_vec = KBToVec(LHS), RHS_vec= KBToVec(RHS);
   if (Exact){
     return VecToKB(ashr_exact_solution(LHS_vec, RHS_vec));
   }else{
     return VecToKB(ashr_solution(LHS_vec, RHS_vec));
-  }
+  }*/
   auto ShiftByConst = [&](const KnownBits &LHS, unsigned ShiftAmt) {
     KnownBits Known = LHS;
     Known.Zero.ashrInPlace(ShiftAmt);
@@ -865,10 +865,10 @@ KnownBits KnownBits::mul(const KnownBits &LHS, const KnownBits &RHS,
   assert(BitWidth == RHS.getBitWidth() && "Operand mismatch");
   assert((!NoUndefSelfMultiply || LHS == RHS) &&
          "Self multiplication knownbits mismatch");
-  auto LHS_vec = KBToVec(LHS), RHS_vec= KBToVec(RHS);
+  /*auto LHS_vec = KBToVec(LHS), RHS_vec= KBToVec(RHS);
   auto res = mul_solution(LHS_vec, RHS_vec);
   auto res_kb = VecToKB(res);
-  return res_kb;
+  return res_kb;*/
   // Compute the high known-0 bits by multiplying the unsigned max of each side.
   // Conservatively, M active bits * N active bits results in M + N bits in the
   // result. But if we know a value is a power-of-2 for example, then this
@@ -1026,12 +1026,12 @@ KnownBits KnownBits::sdiv(const KnownBits &LHS, const KnownBits &RHS,
     Known.setAllZero();
     return Known;
   }
-  auto LHS_vec= KBToVec(LHS), RHS_vec= KBToVec(RHS);
+  /*auto LHS_vec= KBToVec(LHS), RHS_vec= KBToVec(RHS);
   if(Exact){
     return VecToKB(sdiv_exact_solution(LHS_vec, RHS_vec));
   }else{
     return VecToKB(sdiv_solution(LHS_vec, RHS_vec));
-  }
+  }*/
 
   std::optional<APInt> Res;
   if (LHS.isNegative() && RHS.isNegative()) {
@@ -1177,16 +1177,16 @@ KnownBits &KnownBits::operator|=(const KnownBits &RHS) {
 
 KnownBits &KnownBits::operator^=(const KnownBits &RHS) {
   // Result bit is 0 if both operand bits are 0 or both are 1.
-  //APInt Z = (Zero & RHS.Zero) | (One & RHS.One);
+  APInt Z = (Zero & RHS.Zero) | (One & RHS.One);
   // Result bit is 1 if one operand bit is 0 and the other is 1.
-  //One = (Zero & RHS.One) | (One & RHS.Zero);
-  //Zero = std::move(Z);
-  //return *this;
-  auto LHS_vec = KBToVec(*this), RHS_vec= KBToVec(RHS);
+  One = (Zero & RHS.One) | (One & RHS.Zero);
+  Zero = std::move(Z);
+  return *this;
+  /*auto LHS_vec = KBToVec(*this), RHS_vec= KBToVec(RHS);
   auto res = xor_solution(LHS_vec, RHS_vec);
   auto res_kb = VecToKB(res);
   *this = res_kb;
-  return *this;
+  return *this;*/
 }
 
 KnownBits KnownBits::blsi() const {
